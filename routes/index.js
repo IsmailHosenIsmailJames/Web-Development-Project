0dba2de419a0619945357d003e6f79caa055befe
MD. Ismail Hosen James <md.ismailhosenjames@gmail.com>
2023-11-16 00:59:28 +0600

    Home page designed and about page designed

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,28 @@
 var express = require("express");
 var router = express.Router();
-
 var path = require("path");
-router.use("/static", express.static(path.join(__dirname, "public")));
+const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.sendFile(path.join(__dirname, "/../public/home/home.html"));
 });
 
-var x = "/home/ismail/Web-Development-Project/public/";
-
-router.get("/file", (req, res) => {
-  res.sendFile(path.join(x + "home/home.html"));
+router.post("/", (req, res) => {
+  res.send("Got a POST request");
 });
+
+router.put("/user", (req, res) => {
+  res.send("Got a PUT request at /user");
+});
+
+router.delete("/user", (req, res) => {
+  res.send("Got a DELETE request at /user");
+});
+
+// const data = fs.readFileSync("post/postData.json");
+// console.log(JSON.parse(data));
+
+// console.log("kjhks");
 
 module.exports = router;
