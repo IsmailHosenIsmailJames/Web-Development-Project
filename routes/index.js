aad909445a56d80c5f451561ada8a41c081509c0
MD. Ismail Hosen Ismail James <md.ismailhosenismailjames@gmail.com>
2023-11-20 01:26:07 +0600

    Node Done

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,44 @@ router.get("/", function (req, res, next) {
   res.sendFile(path.join(__dirname, "/../public/home/home.html"));
 });
 
-router.post("/", (req, res) => {
-  res.send("Got a POST request");
+router.get("/home", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/home/home.html"));
 });
-
-router.put("/user", (req, res) => {
-  res.send("Got a PUT request at /user");
+router.get("/computer", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/cst/pages/cst.html"));
 });
-
-router.delete("/user", (req, res) => {
-  res.send("Got a DELETE request at /user");
+router.get("/cst", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/cst/pages/cst.html"));
+});
+router.get("/telecom", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/telecom/pages/telecom.html"));
+});
+router.get("/about", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/tpi/pages/about.html"));
+});
+router.get("/students", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/tpi/pages/students.html"));
+});
+router.get("/teachers", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/tpi/pages/teachersInfo.html"));
+});
+router.get("/mechanical", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/mechanical/pages/mechanical.html"));
+});
+router.get("/electronic", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/electronic/pages/electronics.html"));
+});
+router.get("/electrical", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/electrical/pages/electrical.html"));
+});
+router.get("/developer", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/developer/developer.html"));
+});
+router.get("/constraction", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/constraction/pages/constraction.html"));
+});
+router.get("/civil", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/civil/pages/civil.html"));
 });
 
 // const data = fs.readFileSync("post/postData.json");
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var path = require("path");
+const fs = require("fs");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/home/home.html"));
 });
+
+router.get("/home", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/home/home.html"));
+});
+router.get("/computer", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/cst/pages/cst.html"));
+});
+router.get("/cst", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/cst/pages/cst.html"));
+});
+router.get("/telecom", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/telecom/pages/telecom.html"));
+});
+router.get("/about", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/tpi/pages/about.html"));
+});
+router.get("/students", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/tpi/pages/students.html"));
+});
+router.get("/teachers", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/tpi/pages/teachersInfo.html"));
+});
+router.get("/mechanical", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/mechanical/pages/mechanical.html"));
+});
+router.get("/electronic", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/electronic/pages/electronics.html"));
+});
+router.get("/electrical", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/electrical/pages/electrical.html"));
+});
+router.get("/developer", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/developer/developer.html"));
+});
+router.get("/constraction", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/constraction/pages/constraction.html"));
+});
+router.get("/civil", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/../public/civil/pages/civil.html"));
+});
+
+// const data = fs.readFileSync("post/postData.json");
+// console.log(JSON.parse(data));
+
+// console.log("kjhks");
 
 module.exports = router;
